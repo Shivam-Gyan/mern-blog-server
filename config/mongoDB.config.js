db54c6884e58f03e3ac1cdac1e1df390c16e1b87
Shivam Gupta <shivamgupta191092005@gmail.com>
2024-09-11 12:21:04 +0530

    changes in db confige

--- config/mongoDB.config.js
+++ config/mongoDB.config.js
@@ -1,8 +1,8 @@
 import mongooose from 'mongoose'
 
 const db=()=>mongooose
-.connect(process.env.MONGODB_COMPASS)
-// .connect(process.env.MONGODB_URI)
+// .connect(process.env.MONGODB_COMPASS)
+.connect(process.env.MONGODB_URI)
 .then((_)=>console.log("Database connected succefully "))
 .catch(err=>console.log(err.message))
 
